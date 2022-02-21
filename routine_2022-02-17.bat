439a6a31c5e5bb70884c4a01946134d868b07774
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-21 14:33:28 +0800

    Update routine_2022-02-17.bat

--- routine_2022-02-17.bat
+++ routine_2022-02-17.bat
@@ -1,8 +1,5 @@
 @echo off
 
-start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-TIMEOUT /T 30
-
 start python routine_2022-02-17.py --part 4 --parts 6
 start python routine_2022-02-17.py --part 5 --parts 6
 start python routine_2022-02-17.py --part 6 --parts 6
