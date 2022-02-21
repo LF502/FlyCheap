ec2ccdd6f3350b8258b0915fcef7f3b599df2d4d
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-21 16:51:26 +0800

    Update routine_2022-02-17.sh

--- routine_2022-02-17.sh
+++ routine_2022-02-17.sh
@@ -1,1 +1,3 @@
-python3 routine_2022-02-17.py --part 1 --parts 6 & python3 routine_2022-02-17.py --part 2 --parts 6 & python3 routine_2022-02-17.py --part 3 --parts 6 
+cd Documents//GitHub//FlyCheap
+python3 routine_2022-02-17.py --part 1 --parts 3 & python3 routine_2022-02-17.py --part 2 --parts 3 & python3 routine_2022-02-17.py --part 3 --parts 3 
+wait
