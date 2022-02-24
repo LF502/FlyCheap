81c3c6a9760f0764cbfdc5510cc1dcf1cee4b7e5
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-24 23:45:56 +0800

    Remove preproc

--- routine_2022-03-29.py
+++ routine_2022-03-29.py
@@ -1,9 +1,10 @@
 from ctripcrawler import CtripCrawler
-from preprocessor import Preprocessor
+from civilaviation import CivilAviation
 from datetime import date
 from __init__ import Log
 from argparse import ArgumentParser
 import sys
+import pandas
 
 if __name__ == "__main__":
 
@@ -14,8 +15,8 @@ if __name__ == "__main__":
               "URC", "FOC", "TAO", "DLC", "WUH", "CGO", ]
     flightDate = date(2022, 3, 29)
     ignore_threshold = 3
+    airData = CivilAviation()
     ignore_cities = None
-    path = None
     
     parameters = (cities, flightDate, 45, 46, ignore_cities, ignore_threshold)
     
@@ -27,8 +28,25 @@ if __name__ == "__main__":
     parser.add_argument("--parts", type = int, default = 0)
     parse_args = parser.parse_args()
     
+    date_coll = pandas.Timestamp.today().toordinal()
+    new = []
+    header = (
+        'date_flight', 'day_week', 'airline', 'type', 'dep', 
+        'arr', 'time_dep', 'time_arr', 'price', 'price_rate')
+    
     for data in crawler.run(part = parse_args.part, parts = parse_args.parts):
-        if not path:
-            path = crawler.file.parent
-        if not Preprocessor(list = data, path = path, file_name = crawler.file.name).run():
-            print(f'WARN: {crawler.file.name} preprocess skipped...')
+        try:
+            data = pandas.DataFrame(data, columns = header).assign(date_coll = date_coll)
+            data['date_flight'] = data['date_flight'].map(lambda x: x.toordinal())
+            data['day_adv'] = data['date_flight'] - date_coll
+            data['hour_dep'] = data['time_dep'].map(lambda x: x.hour if x.hour else 24)
+            if airData.is_multiairport(crawler.file.name[:3]) or \
+                airData.is_multiairport(crawler.file.name[4:7]):
+                data['route'] = data['dep'].map(lambda x: airData.from_name(x)) + \
+                    '-' + data['arr'].map(lambda x: airData.from_name(x))
+            else:
+                data['route'] = data['dep'] + '-' + data['arr']
+            new.append(data)
+        except:
+            print(f'WARN: {crawler.file.name} merging skipped...')
+    pandas.concat(new).to_csv('merging_2022-03-29.csv', mode = 'a', index = False)
