1d2130eaf75aa37aaae18760d658c06efaf4eac8
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-24 20:46:39 +0800

    Update routine_2022-02-17.bat

--- routine_2022-02-17.bat
+++ routine_2022-02-17.bat
@@ -1,7 +1,8 @@
 @echo off
 
-start python routine_2022-02-17.py --part 4 --parts 6
-start python routine_2022-02-17.py --part 5 --parts 6
-start python routine_2022-02-17.py --part 6 --parts 6
+start python routine_2022-02-17.py --part 1 --parts 4
+start python routine_2022-02-17.py --part 2 --parts 4
+start python routine_2022-02-17.py --part 3 --parts 4
+start python routine_2022-02-17.py --part 4 --parts 4
 
 exit
