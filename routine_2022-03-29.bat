44a95fb8fefa9877e361e61ca6aa4cde7104cd82
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-25 16:00:06 +0800

    Update routine_2022-03-29.bat

--- routine_2022-03-29.bat
+++ routine_2022-03-29.bat
@@ -1,7 +1,8 @@
 @echo off
 
-start python routine_2022-03-29.py --part 1 --parts 3
-start python routine_2022-03-29.py --part 2 --parts 3
-start python routine_2022-03-29.py --part 3 --parts 3
+start python routine_2022-03-29.py --part 1 --parts 4
+start python routine_2022-03-29.py --part 2 --parts 4
+start python routine_2022-03-29.py --part 3 --parts 4
+start python routine_2022-03-29.py --part 4 --parts 4
 
 exit
