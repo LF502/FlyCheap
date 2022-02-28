19780dcfb2841b8830e98ee34babb1fb12d5c846
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-28 15:46:48 +0800

    Delete routine_2022-02-17.bat

--- routine_2022-02-17.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-start python routine_2022-02-17.py --part 1 --parts 4
-start python routine_2022-02-17.py --part 2 --parts 4
-start python routine_2022-02-17.py --part 3 --parts 4
-start python routine_2022-02-17.py --part 4 --parts 4
-
-exit
