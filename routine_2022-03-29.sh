d9e3e68d7cb06704bbfc1214b920134c12ce17d5
Primarie <SyzygyPrimarie@Gmail.com>
2022-02-28 15:47:04 +0800

    Delete routine_2022-03-29.sh

--- routine_2022-03-29.sh
+++
@@ -1,3 +0,0 @@
-cd Documents//GitHub//FlyCheap
-python3 routine_2022-03-29.py --part 1 --parts 3 & python3 routine_2022-03-29.py --part 2 --parts 3 & python3 routine_2022-03-29.py --part 3 --parts 3 
-wait
