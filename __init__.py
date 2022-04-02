d7673a5717c26bb149ba956dd11d57971ab07c12
Primarie <SyzygyPrimarie@Gmail.com>
2022-04-02 22:20:34 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,19 +1,18 @@
-from ctripcrawler import CtripCrawler
-from ctripsearcher import CtripSearcher
+from ctripcrawler import CtripCrawler, CtripSearcher, ItineraryCollector
 from rebuilder import Rebuilder
 import civilaviation
-import sys
-import time
+import sys as _sys
+import time as _time
 
 class Log():
     def __init__(self, logfile: str):
-        self.terminal = sys.stdout
+        self.terminal = _sys.stdout
         self.log = open(logfile, "a", encoding = 'UTF-8',)
  
     def write(self, message):
         self.terminal.write(message)
         if message.startswith("\n") or message.endswith("\n"):
-            self.log.write("\n" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n")
+            self.log.write("\n" + _time.strftime("%Y-%m-%d %H:%M:%S", _time.localtime()) + "\n")
         self.log.write(message)
     def flush(self):
         pass
